88098c6ee70c011ae4b3c61ecfecfdd19a6ef808
jiading <46176148+SmellyTrotter@users.noreply.github.com>
2020-07-18 14:38:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
- echo "hello world!";
-?>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>靓仔的网站</title>
+
+    </head>
+    <body>
+        <h3>欢迎来这里</h3>
+        <?php echo "hello world";?>
+    </body>
+</html>
